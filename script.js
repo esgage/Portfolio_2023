6ca2af6f7453de7610f7052bc1540a371ab40404
Eric Gage <eric@ericgage.us>
2023-05-05 21:23:56 -0400

    set background blur to apply on load

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const projectImages = document.querySelectorAll('.project-image');
-projectImages.forEach((project) => {
-    const projectBackground = project.querySelector('canvas');
+const projectImages = document.querySelectorAll('.project-img-container');
+
+const applyBackground = (project) => {
     const projectImage = project.querySelector('img');
+    const projectBackground = project.querySelector('canvas');
+    projectBackground.setAttribute('width', projectImage.width);
+    projectBackground.setAttribute('height', projectImage.height);
     const ctx = projectBackground.getContext('2d');
     ctx.drawImage(projectImage, 0, 0);
+    console.log('function called');
+}
+
+projectImages.forEach((project) => {
+    const projectImage = project.querySelector('img');
+    if (projectImage.complete){
+        console.log('if');
+        applyBackground(project);
+    } else {
+        projectImage.addEventListener('load', () => {
+            console.log('else');
+            applyBackground(project);
+        });
+    }
 });
